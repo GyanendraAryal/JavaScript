d168907c8671e6ffaabbfac7111a165fe4a9cb27
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-03-26 16:36:30 +0545

    did variables

--- RevisionJS/script.js
+++ RevisionJS/script.js
@@ -1,1 +1,13 @@
+const accountId = 134242;
+let accountEmail = "test@gmail.com";
+var accountPassword = "1254";
+accountCity = "Rupandehi"
+
+console.table([accountEmail, accountCity, accountPassword]);
+
+accountEmail = "test1@gmail.com";
+accountPassword = "9666";
+accountCity = "Gulmi"
+accountPassword = "9666";
+console.table([accountEmail, accountCity, accountPassword]);
 console.log("Here we go again!!")
