7fd2dd64af9bac4ae6d2de3610d74e352937c846
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-03-27 21:18:35 +0545

    data conversion in js

---
+++ RevisionJS/dataConversion.js
@@ -0,0 +1,9 @@
+let score = 33;
+let Score = "33aa";
+
+//console.log(typeof(score));
+console.log(typeof(Score));
+
+let valueInNumber = Number(Score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
