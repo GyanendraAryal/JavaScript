e6d47ea89fd6f9dc60c94c63f8bef2850eed355a
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-03-27 21:06:33 +0545

    practiced datatypes

---
+++ RevisionJS/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//Treat all JS code as newer version
+console.log(8);
+alert(4+6)//Works only in brower
+
+
+//Datatypes
+//Numbers
+//String
+//Object
+//int
+//BigInt
+//null---typeof(Object)
+//undefined---typeof(undefined)
+//symbol---unique
