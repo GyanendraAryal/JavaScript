75b06639e87bafa1c20d3081595f754f2e08390b
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-23 09:30:12 -0700

    practiced type conversion

---
+++ typeConversion.js
@@ -0,0 +1,15 @@
+// // let score = "33adfhah"
+// // console.log(typeof(score))
+
+// // let valinnum = Number(score)
+// // console.log(typeof valinnum)
+// // console.log(valinnum)
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 34
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
