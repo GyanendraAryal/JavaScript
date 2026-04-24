b6ff93099c642ec23898246e5b88b1e4d19fb81c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-25 00:08:36 +0545

    array

---
+++ Array1/array.js
@@ -0,0 +1,2 @@
+const myArr = [1,2,3,3,4,4,true,'hfei']
+console.log(myArr)
--- dataTypes.js
+++ dataTypes.js
@@ -1,20 +1,43 @@
-// // Primitive Datatype-- copy of these are given not the original one
-// // String
-// Number
-// Boolean
-// null
-// undefined
-//Symbol
-//BigInt
+// // // // // // Primitive Datatype-- copy of these are given not the original one
+// // // // // // String
+// // // // // Number
+// // // // // Boolean
+// // // // // null
+// // // // // undefined
+// // // // //Symbol
+// // // // //BigInt
 
-//Reference Type-Non Primitive - direct memory reference is provided
-//Array
-//Object
-//Functions
+// // // // //Reference Type-Non Primitive - direct memory reference is provided
+// // // // //Array
+// // // // //Object
+// // // // //Functions
 
 
-// Strings
-const name = 'Gyanendra'
-const sureName = 'Aryal'
-// console.log(name+sureName)
-console.log(`My name is ${name.toUpperCase()} ${sureName.toLowerCase()}`)
+// // // // // Strings
+// // // // const name = 'Gyanendra'
+// // // // const sureName = 'Aryal'
+// // // // // console.log(name+sureName)
+// // // // console.log(`My name is ${name.toUpperCase()} ${sureName.toLowerCase()}`)
+
+// // // // let str = 'hafefjeicj'
+// // // // let anotherStr = str.substring(0,6)
+// // // // // console.log(anotherStr)
+// // // let str = 'hafefjeicj'
+// // // let sliceStr = str.slice(-2,4)
+// // // console.log(sliceStr)
+
+// // let inpt = '  ahfefjadsif   '
+// // console.log(inpt)
+// // console.log(inpt.trim())
+
+// // let num = 100
+// // console.log(num.toFixed(2))
+
+// // let count = 123.542
+// // console.log(count.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+console.log(Math)
+console.log(Math.abs(-5))
