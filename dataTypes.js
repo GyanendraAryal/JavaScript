28c631278c06bde87d39704460cc7eab583fb11d
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-24 01:52:14 -0700

    about strings

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+// // Primitive Datatype-- copy of these are given not the original one
+// // String
+// Number
+// Boolean
+// null
+// undefined
+//Symbol
+//BigInt
+
+//Reference Type-Non Primitive - direct memory reference is provided
+//Array
+//Object
+//Functions
+
+
+// Strings
+const name = 'Gyanendra'
+const sureName = 'Aryal'
+// console.log(name+sureName)
+console.log(`My name is ${name.toUpperCase()} ${sureName.toLowerCase()}`)
