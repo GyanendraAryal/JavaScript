c28243fd0a9a7e789d36e6be65bcad66a4ea04e1
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-25 13:50:34 +0545

    added additional object practice file

---
+++ Objects/object1.js
@@ -0,0 +1,24 @@
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "Hitesh"
+// }
+// const {coursename,price,courseInstructor} = course//Destructuring of object
+// console.log(coursename);
+// console.log(price);
+// console.log(courseInstructor);
+
+
+//Json and API
+// {
+//     "name":"hitesh",
+//     "course": "js in hindi",
+//     "price": "free"
+// }
+
+
+let myJson = {
+    "name":"hitesh",
+    "course": "js in hindi",
+    "price": "free"
+}
