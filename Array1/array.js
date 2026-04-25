a91b05e0bad70323973ad78d5ea9bd87a6778728
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-25 12:03:32 +0545

    updated array operations logic

--- Array1/array.js
+++ Array1/array.js
@@ -1,2 +1,30 @@
-const myArr = [1,2,3,3,4,4,true,'hfei']
+// const myArr = [1,2,3,true,'hfei']
+// // console.log(myArr)
+
+// myArr.push(8)
+// myArr.push(9)
+// // console.log(myArr)
+
+// myArr.unshift(0)
+// // console.log(myArr)
+
+// // console.log(myArr.includes(8))
+// // console.log(myArr.includes(-8))
+
+// let newArr = myArr.join()
+// // console.log(myArr)
+// // console.log(newArr)
+
+// //Slice and Splice
+
+const myArr = [1,2,3,true,'hfei']
+
+let newSlice = myArr.slice(1,4)
+
 console.log(myArr)
+console.log(newSlice)
+
+
+let newSplice = myArr.splice(1,4)
+console.log(myArr)
+console.log(newSplice)
