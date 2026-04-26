31a13eb4077848c6051ede675c51172a703257b2
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-26 18:10:22 +0545

    added arrow function examples

---
+++ functions/arrow.js
@@ -0,0 +1,23 @@
+// const user = {
+//     username: "Gyanendra",
+//     price: 387542355387,
+//     welcomeMessage: function(){
+//         console.log(`Welcome ${this.username}`);
+//         // console.log(this);
+        
+        
+//     }
+// }
+// // user.welcomeMessage()
+// // user.username = "Dude"
+// // user.welcomeMessage()
+
+// console.log(this);
+
+
+function chai(){
+    const username = "Gyanendra"
+    console.log(this);
+    
+}
+chai()
