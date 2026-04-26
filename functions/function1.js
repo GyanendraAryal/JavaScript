4a36a78981cecc40d3ff2f82e21faaad163619f5
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-26 17:13:55 +0545

    added additional function practice file

---
+++ functions/function1.js
@@ -0,0 +1,23 @@
+// const calculateCartPrice = (...num1) =>{
+//     return num1
+// }
+// console.log(calculateCartPrice(200,200,300,700,600));
+
+
+// let user = {
+//     name: "Gyanendra",
+//     contact: 9876546372
+// }
+// const handleObject = (anyObject) =>{
+//     console.log(`Username is ${anyObject.name} and the contact number is ${anyObject.contact}`);
+    
+// }
+// handleObject(user)
+
+let arr = [1,2,3,4,4,5,6,7,8,9]
+
+const secondItem = (anyArr) =>{
+    return anyArr[2]
+}
+
+console.log(secondItem(arr));
