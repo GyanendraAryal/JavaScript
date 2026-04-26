ff0ee6bdb72f9f5734dbcc5102df9aa7c7eb9eee
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-26 17:13:55 +0545

    updated function logic and examples

--- functions/function.js
+++ functions/function.js
@@ -2,14 +2,31 @@
 //     console.log("My name is Falano!!");
 // }
 // sayMyName()
-import promptSync from "prompt-sync"
+// import promptSync from "prompt-sync"
 
-const prompt = promptSync()
-let a = Number(prompt("Enter number:"))
-let b = Number(prompt("Enter number:"))
+// const prompt = promptSync()
+// let a = Number(prompt("Enter number:"))
+// let b = Number(prompt("Enter number:"))
 
-function addTwoNumbers(num1,num2){
-    return num1 + num2
+// function addTwoNumbers(num1,num2){
+//     return num1 + num2
+// }
+// let result = addTwoNumbers(a,b)
+// console.log(result);
+
+// function addTwoNumbers(num1,num2){
+//     let result = num1 + num2
+//     return result
+// }
+// console.log(addTwoNumbers(a,b))
+
+const loginUserMessage = (username) =>{
+    if(!username){//username === undefined
+        return "Please enter username!!"
+    }
+    else{
+        return `${username} just logged in!!`
+    }
 }
-let result = addTwoNumbers(a,b)
-console.log(result);
+
+console.log(loginUserMessage("Falano"))
