c1c9a1c8415f934d143649ccf38c7d68b5d13d64
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-29 10:45:50 +0545

    feat: add HighOrderArrayloops

---
+++ HighOrderArrayloops/array.js
@@ -0,0 +1,25 @@
+// // //For of Loop
+// // const arry = [1,2,3,4,5,6]
+
+// // for (const num of arry) {
+// //     // const element = arry[i];
+// //     console.log(num);
+// // }
+
+// const greetings = "Hello World"
+
+// for (const greet of greetings){// in returns the iterator and of returns the value
+//     console.log(greet);
+// }
+
+//Map---Object that holds key vlaue pairs
+const map = new Map()
+map.set("NP","Nepal")
+map.set("USA","America")
+map.set("Ger","German")
+// console.log(map);
+
+for(const items of map){
+    console.log(items);
+    
+}
---
+++ HighOrderArrayloops/four.js
@@ -0,0 +1,18 @@
+// const myObject = {
+//     js:"javascript",
+//     cpp:"c++",
+//     rb:"ruby",
+//     swift:"swift by apple"
+// }
+
+// for(key in myObject){
+//     console.log(`Item is ${key}:- ${myObject[key]}`);
+    
+// }
+
+const myProgramming = [6,7,8,9,0]
+
+for(const keys in myProgramming){
+    console.log(`${keys}:-${myProgramming[keys]}`);
+    
+}
