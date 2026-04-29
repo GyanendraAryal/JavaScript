3ce932fa6cc30018ebf17155330b7ca2a1bf01fa
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-29 10:45:50 +0545

    feat: add controlstatement

---
+++ controlstatement/control.js
@@ -0,0 +1,28 @@
+// // const isLoggedIn = true
+// // if(isLoggedIn){
+// //     // console.log("I'm inside if!!");
+// //     console.log("I'm logged in!!");
+    
+// // }
+
+// if(2 == '2'){// == check only the value no the datatype but for strict check use ===
+//     console.log('true');
+// }
+
+//Switch
+// import PromptSync from "prompt-sync";
+// let month = Number(prompt("Enter number 1,2,3"));
+const month = 3
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+        
+        
+}
