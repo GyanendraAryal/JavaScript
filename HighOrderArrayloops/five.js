6a201893b72e1ca6ccc43b568ac5e36550b3cc35
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-04-29 14:36:20 +0545

    fix: update five.js

--- HighOrderArrayloops/five.js
+++ HighOrderArrayloops/five.js
@@ -1,5 +1,15 @@
 const coding = ['js','java','python','cpp']
 
-coding.forEach((element) => {
-    console.log(element);
-});
+// coding.forEach((element) => {
+//     console.log(element);
+// });
+
+// function printMe(val){
+//     console.log(val);
+// }
+// coding.forEach(printMe())
+
+// coding.forEach((items,index,arr)=>{
+//     console.log(items,index,arr);
+    
+// })
