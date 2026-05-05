f34d9904e9f2c3a12ecd168557c2ebecaf39573c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-05 21:34:33 +0545

    api in js

---
+++ ApiRequest/api.js
@@ -0,0 +1,10 @@
+// const prompt = require("prompt");
+
+// let name = prompt("Enter a name")
+
+function myName(name){
+    const repetationWork = setInterval(() => {
+    console.log("Hey Man!!",name);
+}, 1000);
+}
+myName('Gyanendra')
