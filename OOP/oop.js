751354f56e4dc3c61ad8ce789817611e2b444c4f
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-09 12:23:29 +0545

    improved oop/js

--- OOP/oop.js
+++ OOP/oop.js
@@ -1,37 +1,53 @@
-// const user = {
-//     userName: "Gyanendra",
-//     logInCount: 8,
-//     signedIn: true,
+// // const user = {
+// //     userName: "Gyanendra",
+// //     logInCount: 8,
+// //     signedIn: true,
 
-//     getUserDetails: function(){
-//         // console.log("All user detail");
-//         // console.log(`Username: ${this.userName}`);
-//         // console.log(`Username: ${this.userName}`);
-//         console.log(this);
+// //     getUserDetails: function(){
+// //         // console.log("All user detail");
+// //         // console.log(`Username: ${this.userName}`);
+// //         // console.log(`Username: ${this.userName}`);
+// //         console.log(this);
         
-//     }
+// //     }
+// // }
+// // // console.log(user);
+// // // console.log(user.userName);
+// // // console.log(user.getUserDetails());
+// // console.log(this);
+
+// function User(username,loginCount,isLoggedIn){
+//     this.username = username;
+//     this.loginCount = loginCount;
+//     this.isLoggedIn = isLoggedIn;
+
+//     // return this :- by default this is returned
 // }
-// // console.log(user);
-// // console.log(user.userName);
-// // console.log(user.getUserDetails());
-// console.log(this);
+// const userOne = new User("GyanendraAryal", 8, true)
+// // console.log(userOne);
+// // const userTwo = new User("Ram", 3, true)
+// // console.log(userTwo);
+// // const userThree = new User("Hari", 9, false)
+// // console.log(userThree);
+// // const userFour = new User("Shyam", 4, true)
+// // console.log(userFour);
 
-function User(username,loginCount,isLoggedIn){
-    this.username = username;
-    this.loginCount = loginCount;
-    this.isLoggedIn = isLoggedIn;
+// console.log(userOne.constructor);
 
-    // return this :- by default this is returned
+class User {
+    constructor(username,email,password){
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+    encryptPassword(){
+        return `${this.password}abr45`
+    }
+    changeUserName(){
+        return `${this.username.toUpperCase()}`
+    }
 }
-const userOne = new User("GyanendraAryal", 8, true)
-// console.log(userOne);
-// const userTwo = new User("Ram", 3, true)
-// console.log(userTwo);
-// const userThree = new User("Hari", 9, false)
-// console.log(userThree);
-// const userFour = new User("Shyam", 4, true)
-// console.log(userFour);
 
-console.log(userOne.constructor);
-
-
+const chai = new User('Gyanendra','gyan@gmail.com','8347')
+console.log(chai.encryptPassword());
+console.log(chai.changeUserName());
